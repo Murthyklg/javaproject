c1912a8472bda21f6aa502f8b7aa56b72f25ae61
Murthyklg <klgmurthy4797@gmail.com>
2026-03-31 06:00:49 +0000

    added all the files of the project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,37 @@
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        server.createContext("/", new MyHandler());
+
+        server.setExecutor(null);
+        server.start();
+
+        System.out.println("Server started on port " + port);
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+
+            // 🔥 UPDATED MESSAGE (Webhook test)
+            String response = "Webhook Test Successful - Version 1 🚀";
+
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
